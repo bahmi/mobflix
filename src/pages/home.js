b20bb6331fede963977aeeb551cafacc01d5bb24
Bahadur Alam <bahadurmisho@gmail.com>
2020-10-24 11:48:32 +0600

    Add header section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Feature, OptForm } from "../components";
+import { HeaderContainer } from "../containers/header";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from "../containers/jumbotron";
@@ -6,6 +8,27 @@ import { JumbotronContainer } from "../containers/jumbotron";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at anytime.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter you email to create or restart your
+              membership
+            </OptForm.Text>
+            <OptForm.Break />
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
