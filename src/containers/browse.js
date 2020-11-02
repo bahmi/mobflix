a22f3222da4c4e6503b065b43c3b06ead408bcb6
Bahadur Alam <bahadurmisho@gmail.com>
2020-11-02 11:42:12 +0600

    Implement live search functionality

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -6,6 +6,7 @@ import { Card, Header, Loading, Player } from "../components";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
 import { Item } from "../components/accordion/styles/accordion";
+import Fuse from "fuse.js";
 
 export function BrowseContainer({ slides }) {
   const [category, setCategory] = useState("series");
@@ -26,6 +27,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data,title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
